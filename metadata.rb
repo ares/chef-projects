062524f26c6d8033e03bb29fc88688b7f55408ea
Marek Hulan <mhulan@redhat.com>
2016-03-10 13:55:02 +0100

    Fix postgres on EL6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ar3s.cz@gmail.cz'
 license          'All rights reserved'
 description      'Installs/Configures my projects'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '0.4.2'
 
 depends 'git'
 depends	'user', '>= 0.1.0'
