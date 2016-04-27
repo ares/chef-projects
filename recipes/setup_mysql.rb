6f500bc5ab9f7e96f4b182f3a2e16817787e027f
Marek Hulan <mhulan@redhat.com>
2016-04-27 14:28:18 +0200

    Fix requirements

--- recipes/setup_mysql.rb
+++ recipes/setup_mysql.rb
@@ -1,8 +1,8 @@
-mysql_chef_gem 'default' do
+mysql2_chef_gem 'default' do
   action :install
 end
 
-mysql_service 'foo' do
+mysql_service 'default' do
   port '3306'
   initial_root_password 'changeme'
   action [:create, :start]
